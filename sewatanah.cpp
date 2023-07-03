175af3a776d9d035fa56dad8b788512a48ba0122
ILHAMAFANDI7 <138423586+ILHAMAFANDI7@users.noreply.github.com>
2023-07-03 14:39:16 +0700

    Update sewatanah.cpp

--- sewatanah.cpp
+++ sewatanah.cpp
@@ -1,6 +1,48 @@
-#include<iostream>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 
+struct TanahSawah {
+    float luas;
+    string lokasi;
+    float harga;
+    bool terjual;
+};
+
+// Fungsi untuk membandingkan harga tanah sawah (untuk sorting)
+bool comparePrice(const TanahSawah& a, const TanahSawah& b) {
+    return a.harga < b.harga; // Mengubah tanda perbandingan menjadi kurang dari
+}
+
+// Fungsi untuk menampilkan seluruh data tanah sawah
+void displayTanahSawah(const TanahSawah* tanahs, int size) {
+    cout << "Data Tanah Sawah:" << endl;
+    cout << "==============================================================\n";
+    cout << "Lokasi         | Luas (hektar) | Harga per Hektar |  Terjual | \n";
+    cout << "==============================================================\n";
+    for (int i = 0; i < size; i++) {
+        cout.width(13);
+        cout << left << tanahs[i].lokasi << " |";
+        cout.width(14);
+        cout << fixed << tanahs[i].luas << " |";
+        cout.width(17);
+        cout << fixed << tanahs[i].harga << " |";
+        cout.width(10);
+        cout << (tanahs[i].terjual ? "sudah" : "belum") << "|\n";
+    }
+    cout << "==============================================================\n";
+}
+
+// Fungsi untuk melakukan sorting manual
+void manualSort(TanahSawah* tanahs, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (tanahs[j].harga > tanahs[j + 1].harga) {
+                swap(tanahs[j], tanahs[j + 1]);
+            }
+        }
+    }
+}
 int main() {
     const int MAX_TANAH = 5;
     TanahSawah tanahs[MAX_TANAH];
